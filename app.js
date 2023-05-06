614ee14bf1f14eea044c3169ab1ae3d5bfa7de9d
krishnamoorthy2000 <krishnamalathi2000@gmail.com>
2023-05-06 12:30:31 +0530

    first

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+app.listen(5000, () => console.log("Server Running"));
+
+const contactEmail = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.USER_MAIL,
+    pass: process.env.PASSWORD,
+  },
+});
+
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send");
+  }
+});
+
+
+router.post("/contact", (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const message = req.body.message; 
+  const mail = {
+    from: email,
+    to: 'krishnamalathi2000@gmail.com',
+    subject: "Contact Form Submission",
+    html: '<p>Name: ' + name + '</p>\n' +
+        '<p>Email: ' + email + '</p>\n' +
+        '<p>Message: ' + message + '</p>\n',
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json({ status: "ERROR" });
+    } else {
+      res.json({ status: "Message Sent" });
+    }
+  });
+});
